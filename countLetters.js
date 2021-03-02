1985dcf1d4dbb1fad535cff3c39362e109b148ea
David Radvan <david.radvan@gmail.com>
2021-03-02 15:33:23 -0800

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(inputStr) {
+  const noSpaces = inputStr.split(" ").join("").toLowerCase();
+  let outputObj = {};
+
+  for (const char of noSpaces) {
+    if (outputObj[char]) {
+      outputObj[char] += 1;
+    } else {
+      outputObj[char] = 1;
+    }
+  }
+  return outputObj;
+};
+
+console.log(assertEqual(countLetters("lighthouse in the house")["h"], 4));
+console.log(countLetters("lighthouse in the house"));
