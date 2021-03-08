e9fd0f1706169fefc85ecfcdb5eb4567ff952685
David Radvan <david.radvan@gmail.com>
2021-03-08 10:10:48 -0800

    added mocha and chai testing for middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,20 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), [6]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+describe("#head", () => {
+  it("odd numbered array lengths should return middle elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("even numbered array lengths should return two middle elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("odd numbered array lengths should return middle elements: test 2", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]), [6]);
+  });
+
+  it("even numbered array lengths should return two middle elements: test 2", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,34 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(assertEqual(tail(words)[0], words[1]));
-console.log(tail(["oneWord"]));
-console.log(tail([]));
+
+describe("#head", () => {
+  it("tail function should not alter original array", () => {
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("First element of array should be removed", () => {
+    assert.deepEqual(tail(words)[0], words[1]);
+  });
+
+  it("One element arrays should return empty arrays", () => {
+    assert.deepEqual(tail(["oneWord"]), []);
+  });
+
+  it("Should return empty array for empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
+
+
+
+
+
+
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// console.log(assertEqual(tail(words)[0], words[1]));
+// console.log(tail(["oneWord"]));
+// console.log(tail([]));
